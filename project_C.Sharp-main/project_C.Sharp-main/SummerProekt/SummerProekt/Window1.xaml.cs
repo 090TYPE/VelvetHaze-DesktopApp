fcbb88da34b3f6b55f225ee2a1fa4bf97d75d3aa
kazaxx <haligali025@gmail.com>
2024-04-21 21:40:55 +0300

    v.0.1 - Женя сделал первый front
    Co-Authored-By: Akvamarinus <118621346+Akvamarinus@users.noreply.github.com>

---
+++ project_C.Sharp-main/project_C.Sharp-main/SummerProekt/SummerProekt/Window1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SummerProekt
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        private void Vhod_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Reg_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 window2 = new Window2();
+            window2.Show();
+            this.Close();
+        }
+        private void Exit_Button_Click(Object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
